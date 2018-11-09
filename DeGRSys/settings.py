9e60a4c2098870751e5fcf60dd16152289716185
pedro-vostro <vaz.valois@hotmail.com>
2018-11-09 15:47:07 -0200

    dynamicForm deletado.

--- DeGRSys/settings.py
+++ DeGRSys/settings.py
@@ -28,19 +28,26 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
     'widget_tweaks',
     'bootstrapform',
+]
+
+PROJECT_APPS = [
     'registration',
     'labs',
-    # 'dynamicForm',
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- dynamicForm/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from dynamicForm.views import myview, dynamic_form
-
-urlpatterns = [
-    path('', dynamic_form, name='myview'),
-]
