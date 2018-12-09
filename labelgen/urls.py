08ed23992c43ff4c2590f2e840cb3a29c8860af0
Pedrexus <vaz.valois@df.ufscar.br>
2018-12-09 11:21:26 -0200

    dependência desnecessária.

--- labelgen/urls.py
+++ labelgen/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from .views import generate_view
-from .image_gen import graphics
 
 urlpatterns = [
-    path('img', graphics),
     path('<residuo_id>', generate_view, name='pdf_waste'),
 ]
